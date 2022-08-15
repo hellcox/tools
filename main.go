f396df7a54095fbcea5ec23294c19635515694fe
longxiao <longxiao@meross.com>
2022-08-15 18:04:59 +0800

    初始化wails v2项目

--- main.go
+++ main.go
@@ -1,50 +1,33 @@
 package main
 
 import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/widget"
-	"os"
-	"tools/tool/jsontool"
-	"tools/tool/timetool"
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
 )
 
-func init() {
-	os.Setenv("FYNE_FONT", "assets/AlibabaPuHuiTi-2-65-Medium.ttf")
-	os.Setenv("FYNE_SCALE", "1")
-}
-
-var MyWindow fyne.Window
+//go:embed frontend/dist
+var assets embed.FS
 
 func main() {
-	myApp := app.New()
-	icon, _ := fyne.LoadResourceFromPath("assets/icon1.png")
-	myApp.SetIcon(icon)
-	MyWindow := myApp.NewWindow("工具箱")
-	MyWindow.CenterOnScreen()
-	//myApp.Settings().SetTheme(theme.DarkTheme())
+	// Create an instance of the app structure
+	app := NewApp()
 
-	disabled := widget.NewButton("Disabled", func() {})
-	disabled.Disable()
-
-	tabs := container.NewAppTabs(
-		container.NewTabItem("时  间 ", timetool.MakeGridLayout(MyWindow)),
-		container.NewTabItem("JSON ", jsontool.MakeLayout(MyWindow)),
-		container.NewTabItem("布  局 ", MakeLayout(MyWindow)),
-		container.NewTabItem("文  本 ", container.New(layout.NewVBoxLayout(), widget.NewLabel(" xxxxx"))),
-		container.NewTabItem("关  于 ", container.New(layout.NewVBoxLayout(), widget.NewLabel(" Version:\t0.0.1 \n Date:\t2022-08-02 \n Author:\tHellcox"))),
-	)
-	tabs.SetTabLocation(container.TabLocationLeading)
-	tabs.Size()
-
-	MyWindow.Resize(fyne.Size{
-		Width:  750,
-		Height: 400,
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:            "tools",
+		Width:            1024,
+		Height:           768,
+		Assets:           assets,
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
 	})
-	MyWindow.SetMaster()
-	MyWindow.SetContent(tabs)
-	MyWindow.ShowAndRun()
-	os.Unsetenv("FYNE_FONT")
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
 }
